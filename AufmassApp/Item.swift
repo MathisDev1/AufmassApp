2d791c68a967a298c32572ca4553d59fd69da57d
MathisDev1 <mathis.dev.kontakt@gmail.com>
2026-06-09 19:42:50 +0200

    Initial Commit

---
+++ AufmassApp/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  AufmassApp
+//
+//  Created by Mathis Kuhlenkamp on 09.06.26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
